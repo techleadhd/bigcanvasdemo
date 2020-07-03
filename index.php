23adbdeaa50b64a9824ba95bd7bfa783b58cfb46
AsherSeiling <f14asher@gmail.com>
2020-07-03 15:29:37 -0500

    Switching the styleing from the style tag to a stlylesheet, and cleaning up the front-end design.

--- index.php
+++ index.php
@@ -6,56 +6,50 @@ print <<<EOF
 <!doctype html>
 <html>
 <!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-firestore.js"></script>
+<head>
+    <script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-firestore.js"></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="index.js?version=$VERSION"></script>
-
-<style>
-    body {
-        background-color: #f0f0f0;
-        margin: 16px;
-        font-family: Arial, Helvetica, sans-serif;
-    }
-
-    #mycanvas {
-        border: 1px #000 solid;
-        background-color: #fff;
-        cursor: pointer;
-    }
-
-    #mycanvasWrapper {
-        position: relative;
-    }
-
-    #selectedBox {
-        border: 1px rgba(0, 50, 100, 0.5) solid;
-        background-color: rgba(0, 50, 100, 0.25);
-        position: absolute;
-        pointer-events: none;
-    }
-</style>
-<script>
-    let DIMENSION = $DIMENSION;
-</script>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="index.js?version=$VERSION"></script>
+    <link rel="stylesheet" type="text/css" href="index.css">
+</head>
 <body>
-    <h2>Big Canvas Demo</h2>
-
-    <div style='padding:8px;color:#222'>
-    YouTube video walkthrough: <a href=https://youtu.be/t1aXuJkmTg8>https://youtu.be/t1aXuJkmTg8</a><BR>
-    Source code: <a href=https://github.com/techleadhd/bigcanvasdemo>https://github.com/techleadhd/bigcanvasdemo/</a><BR>
-<BR>
-Looking for more coding tips? <BR>
-Join ex-Google/ex-Facebook engineers at
-    <a href=http://techinterviewpro.com>http://techinterviewpro.com</a> for interview prep to FANG companies.
+    <div class="headbar">
+        <br>
+        <h2>Big Canvas Demo</h2>
+        <br>
     </div>
-
-    <div id=mycanvasWrapper>
-        <canvas id=mycanvas></canvas>
-    </div>
-
+    <script>
+        let DIMENSION = $DIMENSION;
+    </script>
+    <center>
+        <div style='padding:8px;color:#222; margin-top: 20pt;'>
+            <p>
+                YouTube video walkthrough: <a href=https://youtu.be/t1aXuJkmTg8>https://youtu.be/t1aXuJkmTg8</a>
+            </p>
+            <BR>
+            <p>
+                Source code: <a href=https://github.com/techleadhd/bigcanvasdemo>https://github.com/techleadhd/bigcanvasdemo/</a>
+            </p>
+            <BR>
+            <BR>
+          
+            </div>
+            <div id=mycanvasWrapper style="margin-top: 25pt;">
+                <canvas id=mycanvas></canvas>
+            </div>
+            <br>
+            <br>
+            <p>
+                Looking for more coding tips?
+            </p>
+            <BR>
+            <p>
+                Join ex-Google/ex-Facebook engineers at
+                <a href=http://techinterviewpro.com>http://techinterviewpro.com</a> for interview prep to FANG companies.
+            </p>
+    </center>
 </body>
 
 </html>
